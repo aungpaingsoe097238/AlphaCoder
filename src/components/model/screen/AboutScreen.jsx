2efcc0bb8a46a3d19aa5406bfac9d45e97b0267d
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2023-02-23 22:29:22 +0630

    about

--- src/components/model/screen/AboutScreen.jsx
+++ src/components/model/screen/AboutScreen.jsx
@@ -5,115 +5,18 @@ import photo from "../../../assets/images/kznm/photo.jpg";
 const AboutScreen = () => {
   return (
     <div className=" overflow-y-auto h-full">
-      {/* <div className="flex">
-        <div className="">
-          <div>Biography</div>
-          <div className=" flex justify-center items-center gap-5">
-            <div className="relative overflow-x-auto">
-              <table className="w-full text-left text-gray-700 text-sm ">
-                <tbody>
-                  <tr>
-                    <td className="py-1">Name</td>
-                    <td>:</td>
-                    <td>Khaing Zar Ni Maw</td>
-                  </tr>
-                  <tr>
-                    <td className="py-1">Email</td>
-                    <td>:</td>
-                    <td>khaingzarnimaw1996@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <td className="py-1">Phone</td>
-                    <td>:</td>
-                    <td>080-5160-7234</td>
-                  </tr>
-                  <tr>
-                    <td className="py-1">Birthday</td>
-                    <td>:</td>
-                    <td>10-6-1996</td>
-                  </tr>
-                  <tr>
-                    <td className="py-1">Gender</td>
-                    <td>:</td>
-                    <td>Female</td>
-                  </tr>
-                  <tr>
-                    <td className="py-1">Address</td>
-                    <td>:</td>
-                    <td>
-                      〒 160-0007 東京都
-                      新宿区荒木町15番地横内マンション302ホンケマンション２０２号
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <div className="flex ">
+        <div className=" basis-1/3">
+          <div className=" about-img"></div>
+        </div>
+        <div className=" basis-2/3">
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            sapiente impedit? Nostrum tempore officiis eum deleniti maxime
+            consequatur, quia distinctio soluta exercitationem sed commodi vitae
+            natus numquam cumque corrupti tempora.
           </div>
         </div>
-      </div> */}
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa neque
-        recusandae harum fuga eaque mollitia reprehenderit, ducimus eligendi
-        magni id repudiandae praesentium ea aut sed nesciunt, nulla facere
-        consequuntur aspernatur!
       </div>
     </div>
   );
