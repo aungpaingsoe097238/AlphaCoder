dbf0167af3c041ce032622bded59ac03143af300
AungPaingSoe <aungpaingsoe097238@gmail.com>
2023-02-23 14:02:36 +0630

    pr

--- src/components/model/screen/AboutScreen.jsx
+++ src/components/model/screen/AboutScreen.jsx
@@ -5,16 +5,50 @@ import photo from "../../../assets/images/kznm/photo.jpg";
 const AboutScreen = () => {
   return (
     <div className="my-3 mx-10">
-      <div>
-        <div>
-          
-        </div>
-        <div>
-          <img
-            src={photo}
-            className=" h-[300px] object-cover rounded-sm shadow-sm "
-            alt=""
-          />
+      <div className="flex ">
+        <div className="">
+          <div>Biography</div>
+          <div className=" flex gap-5">
+            <div>
+              <img src={ photo } className=" h-[200px] rounded-sm write-style" alt="" />
+            </div>
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-left text-gray-800 ">
+                <tbody>
+                  <tr>
+                    <td className="py-1">Name</td>
+                    <td>:</td>
+                    <td>Khaing Zar Ni Maw</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Email</td>
+                    <td>:</td>
+                    <td>khaingzarnimaw1996@gmail.com</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Phone</td>
+                    <td>:</td>
+                    <td>080-5160-7234</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Birthday</td>
+                    <td>:</td>
+                    <td>10-6-1996</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Gender</td>
+                    <td>:</td>
+                    <td>Female</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Address</td>
+                    <td>:</td>
+                    <td>〒 160-0007 東京都 新宿区荒木町15番地横内マンション302ホンケマンション２０２号</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
