01d325fa5270c4762a17d6fbd7e327ab27360ae7
AungPaingSoe <aungpaingsoe097238@gmail.com>
2023-02-24 14:55:31 +0630

    about detail

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -19,42 +19,42 @@ const Gallery = () => {
       <div class="columns-1 md:columns-3 xl:columns-4 ">
         <img
           src={k1}
-          className=" aspect-square md:aspect-video gallery-img "
+          className=" aspect-square gallery-img "
           alt=""
         />
         <img
           src={k2}
-          className=" aspect-square md:aspect-video gallery-img"
+          className=" aspect-square gallery-img"
           alt=""
         />
         <img
           src={k16}
-          className="aspect-square md:aspect-square gallery-img"
+          className="aspect-square gallery-img"
           alt=""
         />
         <img
           src={k5}
-          className=" aspect-square md:aspect-square gallery-img"
+          className=" aspect-square gallery-img"
           alt=""
         />
         <img
           src={k11}
-          className=" aspect-square md:aspect-square gallery-img"
+          className=" aspect-square gallery-img"
           alt=""
         />
         <img
           src={k12}
-          className=" aspect-square md:aspect-square gallery-img"
+          className=" aspect-square gallery-img"
           alt=""
         />
         <img
           src={k14}
-          className=" aspect-square md:aspect-video gallery-img"
+          className=" aspect-square  gallery-img"
           alt=""
         />
         <img
           src={k15}
-          className=" aspect-square md:aspect-video gallery-img"
+          className=" aspect-square gallery-img"
           alt=""
         />
       </div>
--- src/components/model/screen/AboutScreen.jsx
+++ src/components/model/screen/AboutScreen.jsx
@@ -25,151 +25,153 @@ const AboutScreen = () => {
   ];
 
   return (
-    <div className=" overflow-y-auto h-full m-5">
-      <LineTitle title="Biography" />
-      <div className="flex ">
-        <div className=" basis-1/3">
-          <div className="">
-            <img
-              src={photo}
-              className=" h-64 mx-auto rounded-sm shadow-sm "
-              alt=""
-            />
+    <div className=" w-full overflow-y-auto h-full">
+      <div className=" m-5">
+        <LineTitle title="Biography" />
+        <div className="flex ">
+          <div className=" basis-1/3">
+            <div className="">
+              <img
+                src={photo}
+                className=" h-64 mx-auto rounded-sm shadow-sm "
+                alt=""
+              />
+            </div>
+            <div className="my-2">
+              <div className=" flex justify-center items-center gap-5 text-slate-700">
+                <BsFacebook className=" text-xl cursor-pointer" />
+                <BsInstagram className=" text-xl cursor-pointer" />
+                <BsLine className=" text-xl cursor-pointer" />
+                <BsGithub className=" text-xl cursor-pointer" />
+              </div>
+            </div>
           </div>
-          <div className="my-2">
-            <div className=" flex justify-center items-center gap-5 text-slate-700">
-              <BsFacebook className=" text-xl cursor-pointer" />
-              <BsInstagram className=" text-xl cursor-pointer" />
-              <BsLine className=" text-xl cursor-pointer" />
-              <BsGithub className=" text-xl cursor-pointer" />
+          <div className=" basis-2/3">
+            <div className=" flex flex-col gap-2">
+              <div>
+                <table className=" text-sm text-slate-700">
+                  <tbody>
+                    {table_abouts?.map((list) => (
+                      <tr>
+                        <td className="py-1">{list.title}</td>
+                        <td>:</td>
+                        <td>{list.text}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <button className=" py-2 px-3 border border-slate-700 rounded-sm hover:bg-slate-700 hover:text-white ">
+                  Download CV
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        <div className=" basis-2/3">
-          <div className=" flex flex-col gap-2">
-            <div>
-              <table className=" text-sm text-slate-700">
-                <tbody>
-                  {table_abouts?.map((list) => (
-                    <tr>
-                      <td className="py-1">{list.title}</td>
-                      <td>:</td>
-                      <td>{list.text}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        <LineTitle title="Programming" />
+        <div className=" flex">
+          <div className=" basis-1/3">
+            <div className="flex flex-col gap-3 text-slate-700">
+              <ul className=" flex justify-center gap-6">
+                <li>
+                  <div className=" w-[50px] text-center">
+                    <AiFillHtml5 className=" text-2xl mx-auto" />
+                    <span className="text-xs">Html</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <IoLogoCss3 className=" text-2xl mx-auto" />
+                    <span className="text-xs">Css</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <IoLogoJavascript className=" text-2xl mx-auto" />
+                    <span className="text-xs">JavaScript</span>
+                  </div>
+                </li>
+              </ul>
+              <ul className=" flex justify-center gap-6">
+                <li>
+                  <div className=" w-[50px] text-center">
+                    <SiBootstrap className=" text-2xl mx-auto" />
+                    <span className="text-xs">Bootstrap5</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <SiTailwindcss className=" text-2xl mx-auto" />
+                    <span className="text-xs">TailwindCss</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <SiReact className=" text-2xl mx-auto" />
+                    <span className="text-xs">React</span>
+                  </div>
+                </li>
+              </ul>
+              <ul className=" flex justify-center gap-6">
+                <li>
+                  <div className=" w-[50px] text-center">
+                    <TbHash className=" text-2xl mx-auto" />
+                    <span className="text-xs">C#</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <GrMysql className=" text-2xl mx-auto" />
+                    <span className="text-xs">MySQL</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="w-[50px]  text-center">
+                    <SiMongodb className=" text-2xl mx-auto" />
+                    <span className="text-xs">MongoDB</span>
+                  </div>
+                </li>
+              </ul>
             </div>
-            <div>
-              <button className=" py-2 px-3 border border-slate-700 rounded-sm hover:bg-slate-700 hover:text-white ">
-                Download CV
-              </button>
+          </div>
+          <div className=" basis-1/3">
+            <div className=" text-lg">Other</div>
+            <p className=" text-slate-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              voluptatibus consectetur dolorum omnis. Laboriosam, obcaecati!
+              Nostrum officiis neque repellat, dolorem ea voluptates aut. Omnis
+              ex eos eveniet. Similique, perspiciatis itaque.
+            </p>
+          </div>
+        </div>
+        <LineTitle title="Education" />
+        <div className=" flex">
+          <div className=" basis-1/3">
+            <div className=" text-center text-slate-700">
+              <div className=" flex flex-col gap-4">
+                <div>2012年10月 〜 2016年10月</div>
+                <div>2019年3月 〜 2021年3月</div>
+                <div>2021年4月 〜 2023年3月</div>
+              </div>
+            </div>
+          </div>
+          <div className=" basis-2/3">
+            <div className=" flex flex-col gap-4 text-slate-700">
+              <div>
+                B.A(Geography) Yangon University of Distance Education (Dagon
+                branch)
+              </div>
+              <div>国際言葉学院</div>
+              <div>
+                専門学校静岡電子情報カレッジ IT スペシャリスト IT
+                ゲームロボット＆システム学科
+              </div>
             </div>
           </div>
         </div>
+        <LineTitle/>
       </div>
-      <LineTitle title="Programming" />
-      <div className=" flex">
-        <div className=" basis-1/3">
-          <div className="flex flex-col gap-3 text-slate-700">
-            <ul className=" flex justify-center gap-6">
-              <li>
-                <div className=" w-[50px] text-center">
-                  <AiFillHtml5 className=" text-2xl mx-auto" />
-                  <span className="text-xs">Html</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <IoLogoCss3 className=" text-2xl mx-auto" />
-                  <span className="text-xs">Css</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <IoLogoJavascript className=" text-2xl mx-auto" />
-                  <span className="text-xs">JavaScript</span>
-                </div>
-              </li>
-            </ul>
-            <ul className=" flex justify-center gap-6">
-              <li>
-                <div className=" w-[50px] text-center">
-                  <SiBootstrap className=" text-2xl mx-auto" />
-                  <span className="text-xs">Bootstrap5</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <SiTailwindcss className=" text-2xl mx-auto" />
-                  <span className="text-xs">TailwindCss</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <SiReact className=" text-2xl mx-auto" />
-                  <span className="text-xs">React</span>
-                </div>
-              </li>
-            </ul>
-            <ul className=" flex justify-center gap-6">
-              <li>
-                <div className=" w-[50px] text-center">
-                  <TbHash className=" text-2xl mx-auto" />
-                  <span className="text-xs">C#</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <GrMysql className=" text-2xl mx-auto" />
-                  <span className="text-xs">MySQL</span>
-                </div>
-              </li>
-              <li>
-                <div className="w-[50px]  text-center">
-                  <SiMongodb className=" text-2xl mx-auto" />
-                  <span className="text-xs">MongoDB</span>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className=" basis-1/3">
-          <div className=" text-lg">Other</div>
-          <p className=" text-slate-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-            voluptatibus consectetur dolorum omnis. Laboriosam, obcaecati!
-            Nostrum officiis neque repellat, dolorem ea voluptates aut. Omnis ex
-            eos eveniet. Similique, perspiciatis itaque.
-          </p>
-        </div>
-      </div>
-      <LineTitle title="Education" />
-      <div className=" flex">
-        <div className=" basis-1/3">
-          <div className=" text-center">
-            <div className=" flex flex-col gap-4">
-              <div>2012年10月 〜 2016年10月</div>
-              <div>2019年3月 〜 2021年3月</div>
-              <div>2021年4月 〜 2023年3月</div>
-            </div>
-          </div>
-        </div>
-        <div className=" basis-2/3">
-          <div className=" flex flex-col gap-4">
-            <div>
-              B.A(Geography) Yangon University of Distance Education (Dagon
-              branch)
-            </div>
-            <div>国際言葉学院</div>
-            <div>
-              専門学校静岡電子情報カレッジ IT スペシャリスト IT
-              ゲームロボット＆システム学科
-            </div>
-          </div>
-        </div>
-      </div>
-      <LineTitle title="Experience" />
     </div>
   );
 };
