46578bf6bfd13499cc602a1945cdaa3ec51a2a4f
Aung Paing Soe <90528236+aungpaingsoe097238@users.noreply.github.com>
2023-05-09 21:16:30 +0630

    social

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Undraw3 from "../assets/images/undrew/undraw3.png";
-import Undraw4 from "../assets/images/undrew/undraw2.png";
+import Undraw4 from "../assets/images/undrew/undraw4.png";
 import Undraw1 from "../assets/images/undrew/undraw1.png";
 import Title from "./utli/Title";
 
@@ -10,20 +10,20 @@ const Services = () => {
       title: "Web Design",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur",
-      img: Undraw3
+      img: Undraw3,
     },
     {
       title: "Mobile Development",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur",
-      img: Undraw4
+      img: Undraw4,
     },
     {
       title: "Web Application",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur",
-      img: Undraw1
-    }
+      img: Undraw1,
+    },
   ];
 
   return (
@@ -33,24 +33,23 @@ const Services = () => {
     >
       <Title title="Services" subTitle="What can I do" />
       <div className="flex flex-col md:flex-row my-10 gap-5">
-
-        {
-          services?.map(service => {
-            <p>{service.title}</p>
-          })
-        }
-
-        <div className=" basis-1/3 shadow-sm border border-gray-200 rounded-lg py-3 px-2  flex flex-col justify-center items-center">
-          <div className="">
-            <img src={services[0].img} alt="" className=" h-36" />
-          </div>
-          <div className=" text-center mt-auto">
-            <span className=" font-medium italic ">{services[0].title}</span>
-            <p className=" text-gray-500 text-sm my-2">
-              {services[0].description}
-            </p>
-          </div>
-        </div>
+        {services?.map((service) => {
+          return (
+            <div className=" basis-1/3 shadow-sm border border-gray-200 rounded-lg py-3 px-2  flex flex-col justify-center items-center">
+              <div className="">
+                <img src={service.img} alt="" className=" h-36" />
+              </div>
+              <div className=" text-center mt-auto">
+                <span className=" font-medium italic ">
+                  {service.title}
+                </span>
+                <p className=" text-gray-500 text-sm my-2">
+                  {service.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import { BsFacebook, BsInstagram, BsGithub } from "react-icons/bs";
 
 const Social = () => {
+  const socials = [
+    { icon : "BsFacebook", link : "https://www.facebook.com/aung.p.soe.5437" }, 
+    { icon : "BsInstagram", link : "https://www.instagram.com/aung.p.soe.532" }, 
+    { icon : "BsGithub", link : "https://github.com/aungpaingsoe097238" }
+  ];
+
   return (
     <ul className=" flex md:flex-col gap-10">
-      <li>
-        <a href="">
-          <BsFacebook className="text-xl cursor-pointer" />
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <BsInstagram className="text-xl cursor-pointer" />
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <BsGithub className="text-xl cursor-pointer" />
-        </a>
-      </li>
+      {socials.map((social) => {
+        const IconComponent = eval(social.icon); // Use eval to convert the icon name to a component
+        return (
+          <li>
+            <a href={ social.link } target="_black">
+              <IconComponent className="text-xl cursor-pointer"  />
+            </a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
